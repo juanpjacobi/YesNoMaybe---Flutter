ad5d59b09d60b52de216121513457a011542fd42
= <=>
2023-11-11 13:02:46 -0300

    Primer commit

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_messages_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _AppBar(),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _AppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: const Padding(
+        padding: EdgeInsets.all(4.0),
+        child: CircleAvatar(
+          backgroundImage: NetworkImage(
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqB7XOKidu1WCdrNfNeCJvD81KSFPfkhlpuRuVhFj8wT6cVa3CCYK6j5TF0AHQckaaDfA&usqp=CAU'),
+        ),
+      ),
+      title: const Text('Mi amor s2'),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                    controller: chatProvider.chatScrollController,
+                    itemCount: chatProvider.messageList.length,
+                    itemBuilder: (context, index) {
+                      final message = chatProvider.messageList[index];
+                      return (message.fromWho == FromWho.her)
+                          ? HerMessageBubble(message: message)
+                          : MyMessageBubble(message: message);
+                    })),
+            MessageFieldBox(
+              onValue: chatProvider.sendMessage,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
